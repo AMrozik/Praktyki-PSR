28db45b3fa3c7df21c5578996c897185ed481487
Andrzej Mrozik <andrzej.mrozik98@gmail.com>
2020-12-14 14:50:30 +0100

    zajecia 14-12-2020

---
+++ src/cwiczenia5/Main.java
@@ -0,0 +1,25 @@
+package cwiczenia5;
+
+public class Main {
+
+    String encode(String message){
+
+//        zaszyfruj message tutaj
+
+        return encoded_Message;
+    }
+
+
+    public static void main(String[] args) {
+
+        int[] tab = {2, 5, 6, 8, 3, 7, 9, 4, 7, 8, 56, 4, 7};
+
+        String s = "8u389jkfsdhifgku";
+
+        char[] arr = s.toCharArray();
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
